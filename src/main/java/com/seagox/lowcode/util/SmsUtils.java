84eb32e1abc76f2a09c5b50d8dd1d937ac6aee80
tiny <tinybrief@gmail.com>
2026-06-23 10:06:30 +0800

    123

--- src/main/java/com/seagox/lowcode/util/SmsUtils.java
+++ src/main/java/com/seagox/lowcode/util/SmsUtils.java
@@ -18,8 +18,8 @@ public class SmsUtils {
 			// 初始化ascClient需要的几个参数
 			final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
 			final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
-			final String accessKeyId = "LTAI2nhQTtX2CkGr";
-			final String accessKeySecret = "BpyIgdbOCcjkViLBaWaBee5s4FiD4v";
+			final String accessKeyId = "123";
+			final String accessKeySecret = "123";
 			// 初始化ascClient,暂时不支持多region（请勿修改）
 			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
 			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
